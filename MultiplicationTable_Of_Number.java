b81b6448b2fb76dea3bdfbda3a3022e13b40ad55
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-11-24 20:09:40 +0530

    close scanner comment

--- MultiplicationTable_Of_Number.java
+++ MultiplicationTable_Of_Number.java
@@ -12,5 +12,13 @@ public class MultiplicationTable_Of_Number {
         for (int i = 1; i <= 12; i++) {
             System.out.println(num + " x " + i + " = " + (num * i));
         }
+
+        // close scanner
+        sc.close();
     }
 }
+
+// why close the Scanner ?
+// over time by time, memory will full of objects.
+// this will cause to crash programme, because out of memory
+// (not affected in small programmes)
