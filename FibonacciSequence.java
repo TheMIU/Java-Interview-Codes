e3ff4ad79b7c9188302ec008b92ca06aadf95cf0
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2023-12-05 15:22:26 +0530

    Fibonacci Sequence

---
+++ FibonacciSequence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers do you want ? : ");
+        int count = sc.nextInt();
+
+        printFibonacciSequence(count);
+    }
+
+    private static void printFibonacciSequence(int count) {
+        int first = 0, second = 1;
+        System.out.print("First " + count + " Numbers of Fibonacci Sequence: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
